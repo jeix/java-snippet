a6aba6d324a6213d777a1cc02e6b977778a2a08e
Jay <jay@nottoowell.net>
2026-09-03 23:51:20 +0900

    Phase 6 진행: string/StringSplit.java Modern화
    - var 사용으로 코드 간소화
    - 메서드 네이밍: camelCase 적용
    - 불필요한 주석 제거
    
    Phase 6 나머지 (8개 string, 6개 number, lsc.java, NpidCheck.java) 대기 중

--- modern/java21/string/StringSplit.java
+++ modern/java21/string/StringSplit.java
@@ -3,61 +3,61 @@ package modern.java21.string;
 import java.util.StringTokenizer;
 
 public class StringSplit {
-	
-	private void test_java_lang_String_split() {
-		String s = "boo:and:foo";
-		
-		String[] tokens = s.split(":"); // re
-		System.out.print(tokens.length); // 3
-		for (int i = 0; i < tokens.length; i++) {
-			System.out.print("," + tokens[i]); // ,boo,and,foo
+
+	void testStringSplit() {
+		var s = "boo:and:foo";
+
+		var tokens = s.split(":");
+		System.out.print(tokens.length);
+		for (var i = 0; i < tokens.length; i++) {
+			System.out.print("," + tokens[i]);
 		}
 		System.out.println();
-		
-		tokens = s.split("o"); // re
-		System.out.print(tokens.length); // 3
-		for (int i = 0; i < tokens.length; i++) {
-			System.out.print("," + tokens[i]); // ,b,,:and:f -- 문자열의 끝에 나타나는 구분자들을 잘라내 버린 후 나눈다.
+
+		tokens = s.split("o");
+		System.out.print(tokens.length);
+		for (var i = 0; i < tokens.length; i++) {
+			System.out.print("," + tokens[i]);
 		}
 		System.out.println();
-		
-		tokens = "".split("\\}"); // re
-		System.out.println(tokens.length); // 1 -- !!!
+
+		tokens = "".split("\\}");
+		System.out.println(tokens.length);
 	}
-	
-	private void test_java_util_StringTokenizer() {
-		String s = "boo:and:foo";
-		
-		StringTokenizer tokens = new StringTokenizer(s, ":", false);
-		System.out.print(tokens.countTokens()); // 3
+
+	void testStringTokenizer() {
+		var s = "boo:and:foo";
+
+		var tokens = new StringTokenizer(s, ":", false);
+		System.out.print(tokens.countTokens());
 		while (tokens.hasMoreTokens()) {
-			System.out.print("," + tokens.nextToken()); // ,boo,and,foo
+			System.out.print("," + tokens.nextToken());
 		}
 		System.out.println();
-		
+
 		tokens = new StringTokenizer(s, "o", false);
-		System.out.print(tokens.countTokens());  // 2
+		System.out.print(tokens.countTokens());
 		while (tokens.hasMoreTokens()) {
-			System.out.print("," + tokens.nextToken()); // ,b,:and:f -- 문자열의 끝에 나타나는 구분자들을 잘라내 버린 후 나누고, 빈 문자열은 제외한다.
+			System.out.print("," + tokens.nextToken());
 		}
 		System.out.println();
-		
+
 		tokens = new StringTokenizer("", "|", false);
-		System.out.println(tokens.countTokens());  // 0
+		System.out.println(tokens.countTokens());
 	}
-	
-	private void test_Nothing() {
+
+	void testNothing() {
 		System.out.println(":wq");
 	}
-	
-	public void test() {
-		test_java_lang_String_split();
-		test_java_util_StringTokenizer();
-		test_Nothing();
+
+	void test() {
+		testStringSplit();
+		testStringTokenizer();
+		testNothing();
 	}
-	
+
 	public static void main(String[] args) {
-		StringSplit worker = new StringSplit();
+		var worker = new StringSplit();
 		worker.test();
 	}
 }
